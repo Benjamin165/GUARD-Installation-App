13dfeb186abe77bacd221b3060f9a5b9691ae4cd
Benjamin165 <52745596+Benjamin165@users.noreply.github.com>
2020-11-10 17:28:12 +0100

    new page

--- guard-installation-app/src/app/app.module.ts
+++ guard-installation-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
