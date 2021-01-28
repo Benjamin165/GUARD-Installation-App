4db15ba0b56a3d331d08a9463b1eb03c834b99ff
Benjamin165 <52745596+Benjamin165@users.noreply.github.com>
2021-01-28 23:25:06 +0100

    build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoic2Fsb25nbyIsImEiOiJja2toNnI4MzgxczY0MzBvYzV6MGhhNjBzIn0.d4PrxHUVUgK5knZk8G_hLg'
+  }
 };
